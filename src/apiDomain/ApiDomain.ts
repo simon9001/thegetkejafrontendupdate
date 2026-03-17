a137aace7393cbcad15b8234bbd86c1e69f8bfdc
simon9001 <simongatungo300@gmail.com>
2026-03-17 19:54:17 +0300

    efo

--- src/apiDomain/ApiDomain.ts
+++ src/apiDomain/ApiDomain.ts
@@ -1,5 +1,5 @@
 const isProd = import.meta.env.PROD;
 
 export const apiDomain = isProd
-    ? 'https://houserentbackend-h6xy.onrender.com/api' // Replace when ready
+    ? 'https://houserentbackend-h6xy.onrender.com/api'
     : 'http://localhost:8000/api';
