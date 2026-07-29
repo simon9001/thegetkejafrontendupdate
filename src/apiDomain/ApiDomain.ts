62a2131bef93baf7991607795751c89d39021ee4
Simon Gatungo <simongatungo300@gmail.com>
2026-07-29 14:19:05 +0300

    Update production API domain URL
    THERE ARE UPDATES

--- src/apiDomain/ApiDomain.ts
+++ src/apiDomain/ApiDomain.ts
@@ -1,5 +1,5 @@
 const isProd = import.meta.env.PROD;
 
 export const apiDomain = isProd
-    ? 'https://houserentbackend-h6xy.onrender.com/api'
+    ? 'https://getkejaupdate.onrender.com/api'
     : 'http://localhost:8000/api';
